4b20ff1d89404a4157a80b8d9aa581a24eb1597e
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-03-01 12:08:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const readline=require('read line');
+const r1=readlne.createinterface({input:process.stdin,output:process.stdout});
+r1.on('line',(year)=>
+	{
+		let y=year;
+		if (y%4==0)
+		{
+		console.log("366")	;
+		}
+		else
+			console.log("365");
+	})
