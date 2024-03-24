0729b35b216b8b283806c58f30ff38d7bb7eddc2
Vyshnavi1986 <157476467+Vyshnavi1986@users.noreply.github.com>
2024-03-24 18:27:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-//your JS code here. If required.
-const readline=require('readline');
-const r1=readlne.createinterface({input:process.stdin,output:process.stdout});
-r1.on('line',(year)=>
-	{
-		let y=year;
-		if (y%4==0)
-		{
-		window.alert("366")	;
-		}
-		else
-			window.alert("365");
-	});
+// program to check leap year
+function checkLeapYear(year) {
+
+    //three conditions to find out the leap year
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
+
+// take input
+const year = prompt('Enter a year:');
+
+checkLeapYear(year);
